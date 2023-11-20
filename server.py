43ae1c32df483bd64181caa8e6661069f86aee13
Henry Chadban on Raspberry Pi <henryj.chadban@gmail.com>
2023-11-20 23:27:19 +1100

    built a server that can cleanly exit in response to an exit command

--- server.py
+++ server.py
@@ -2,20 +2,36 @@
 
 import socket
 import time
-from threading import Timer
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('',5000))
-s.listen(5)
-print("Server is now running.")
-
-def background_controller():
-    message = "Hello Client"
+def create_server():
+    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket uses IPv4 IP addresses and TCP protocol
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #ensure we can reuse the socket
+    #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('',30701)) #bind socket to localhost, port 30699
+    server.listen(5) #backlog = 5, only one client may connect to this server at a time
+    print("Server is now running. Waiting for client to connect . . .")
+    client_socket, client_address = server.accept() #wait for a connection from a client
+    print("Accepted connection from ",client_address[0],":",client_address[1])
+    while True:
+        request = client_socket.recv(1024)
+        request = request.decode('utf-8')
+        print("recieved message : ",request)
+        if request=="exit":
+            send_message(client_socket,"exited")
+            break
+        else:
+            send_message(client_socket,"accepted")
+    
+    #server has finished
+    server.close()
+    client_socket.close()
+    print("connection to client closed, turning off server . . .")
+    
+def send_message(client_socket,message):
     print("Message Sent : ", message)
-    clientsocket.send(bytes(message,"utf-8"))
-    Timer(5, background_controller).start()
+    client_socket.send(bytes(message,"utf-8"))
 
-while True:
-    clientsocket, address = s.accept()
-    print("connection from ", address, "has been established.")
-    background_controller()
+
+if __name__ == '__main__':
+    create_server()
