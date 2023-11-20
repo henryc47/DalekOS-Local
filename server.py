7118dedd6f0ee9cc60be84dc0b7dfb34407aa1c4
Henry Chadban on Raspberry Pi <henryj.chadban@gmail.com>
2023-11-20 18:59:26 +1100

    organised the code so there is no code outside of a block

--- server.py
+++ server.py
@@ -4,18 +4,20 @@ import socket
 import time
 from threading import Timer
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('',5000))
-s.listen(5)
-print("Server is now running.")
+def create_server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('',5006))
+    s.listen(5)
+    print("Server is now running.")
+    while True:
+        clientsocket, address = s.accept()
+        print("connection from ", address, "has been established.")
+        send_message(clientsocket)
 
-def background_controller():
+def send_message(clientsocket):
     message = "Hello Client"
     print("Message Sent : ", message)
     clientsocket.send(bytes(message,"utf-8"))
-    Timer(5, background_controller).start()
 
-while True:
-    clientsocket, address = s.accept()
-    print("connection from ", address, "has been established.")
-    background_controller()
+if __name__ == '__main__':
+    create_server()
